2fd3cfad661b7a28b0770a9b8215661e195d8ad5
kokovas <astemir@e-magic.org>
2013-11-20 10:24:49 +0300

    SAND-116.[@teet, @islam]. Создание главного меню к игре

---
+++ Balls/Assets/Scripts/Main_menu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main_menu : MonoBehaviour {
+	int i=0, j=0;		// итераторы для циклов
+	int numb=29;		// количество уровней в игре
+
+	void OnGUI() {
+		// создаем окно главного меню игры
+		GUI.Box(new Rect(Screen.width/2-350, Screen.height/2-250, 700, 500), "МЕНЮ");
+
+		// создаем кнопки-уровни
+		for (i=0; i<=numb/5; i++) {
+			for (j=0; j<=4; j++) {
+				if (GUI.Button(new Rect(Screen.width/2-315+j*130, Screen.height/2-170+i*50, 100, 40), "Уровень_"+(i*5+j+1))) { 
+					Application.LoadLevel("Game");	
+				}
+			}
+		}
+
+		// выход из игры
+		if (GUI.Button(new Rect(Screen.width/2-100, Screen.height/2+195, 200, 40), "ВЫХОД")) { 
+			// выходим из игры 
+			Application.Quit();
+		}
+	}
+}
